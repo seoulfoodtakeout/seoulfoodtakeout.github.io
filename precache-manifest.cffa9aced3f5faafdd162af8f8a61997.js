ba162e585a04001e6022fc9bae19de1beebdcf65
Yong Lee <developer@yonglee.ca>
2019-02-13 12:27:38 -0500

    Updates

---
+++ precache-manifest.cffa9aced3f5faafdd162af8f8a61997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c60888eff4d2dbfd1a5f",
+    "url": "/static/js/main.c60888ef.chunk.js"
+  },
+  {
+    "revision": "a237d65f63a1f6a702b7",
+    "url": "/static/js/1.a237d65f.chunk.js"
+  },
+  {
+    "revision": "c60888eff4d2dbfd1a5f",
+    "url": "/static/css/main.ec31aa8a.chunk.css"
+  },
+  {
+    "revision": "a02639760a3a6d1dd67d7784b997e9f2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57eb8682f1d6d6b53837729c2dbf8f3e.js"
+  "/precache-manifest.cffa9aced3f5faafdd162af8f8a61997.js"
 );
 
 workbox.clientsClaim();
