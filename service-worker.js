448fb4fcdcadce135c7a439e73cecfccb12df4cc
Yong Lee <developer@yonglee.ca>
2019-03-01 21:17:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.035ba60dcb54862b8d44285328e04f54.js"
+  "/precache-manifest.b12afc5349ecaa69e3722c3284640e76.js"
 );
 
 workbox.clientsClaim();
